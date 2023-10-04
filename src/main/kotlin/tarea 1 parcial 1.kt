e0ade77f03e7bd97609c1f2b3bc5ad01592f072f
thisladcalledmathew <emi_trex@hotmail.com>
2023-10-03 21:49:44 -0600

    them.

---
+++ src/main/kotlin/tarea 1 parcial 1.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+ val numero = 3.14159
+    println(numero)
+    println("el perimetro de un circulo es pi por diametro")
+    val perimetro = numero * 2
+    println(perimetro)
+}
